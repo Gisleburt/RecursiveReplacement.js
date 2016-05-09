32ed14c81f6912a8a4fc8edf80f7888619215e65
Daniel Mason <daniel@danielmason.com>
2016-05-10 00:27:31 +0100

    Added empty methods to roughly match spec

--- src/recursive-replacement/recursive-replacement.ts
+++ src/recursive-replacement/recursive-replacement.ts
@@ -1,28 +1,65 @@
-export interface RecursiveData {
-    [key: string]: string|RecursiveData;
-}
-
-export class RecursiveReplacement {
-
-    protected replacementData:RecursiveData;
-    
-    protected delimiters = /\{\{[^}]+}}/;
+module RecursiveReplacement {
 
     /**
-     * Construct a RecursiveReplacement object
-     * @param {{}}replacementData An object containing strings used for replacement
+     * An array of string keys to either string keys or other arrays
      */
-    constructor(replacementData:RecursiveData) {
-        this.replacementData = replacementData;
+    export interface Data {
+        [key:string]:string|Data;
     }
 
     /**
-     * 
-     * @param testString
+     * An array of string keys to string values
      */
-    public static doesStringContainReplacements(testString:string)
-    {
-        
+    export interface Replacements {
+        [ke:string]:string;
     }
 
+    /**
+     * A collection of functions that will repeatedly make replacements in a string until no more replacements
+     * can be made
+     */
+    export class RecursiveReplacement {
+
+        /**
+         * Does the string contain any delimiters
+         * @param {string} testString The string to checl
+         * @param {string} delimiter Defaults to "{{ }}", must have a single space to separate beginning and end
+         * @return {boolean}
+         */
+        public static doestStringContainReplacements(testString:string, delimiter:string = '{{ }}') {
+
+        }
+
+        /**
+         * This will check for any replacements using the given delimiter and return an of names. The array will be
+         * empty if there were no replacements found.
+         * @param {string} testString The string to search for replacements
+         * @param {string} delimiter Defaults to "{{ }}", must have a single space to separate beginning and end
+         * @return {string[]}
+         */
+        public static getReplacementsInString(testString:string, delimiter:string = '{{ }}') {
+
+        }
+
+        /**
+         * This will replace strings
+         * @param {string} stringWithReplacements The string to do the replacements in
+         * @param {string} delimiter Defaults to "{{ }}", must have a single space to separate beginning and end
+         * @param replacements
+         */
+        public static replaceInString(stringWithReplacements:string, replacements:Replacements, delimiter:string = '{{ }}') {
+
+        }
+
+        /**
+         * Get the string value from the recursive data object
+         * @param key
+         * @param replacementData
+         */
+        public static getReplacementFromData(key:string, replacementData:Data) {
+
+        }
+    }
 }
+
+
