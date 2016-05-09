3f96a6b58d1e87ef91071007194bef04a46e4106
Daniel Mason <daniel@danielmason.com>
2016-05-09 08:14:34 +0100

    Placeholders for Jasmine tests

---
+++ tests/src/recursive-replacement/recursive-replacement.spec.ts
@@ -0,0 +1,36 @@
+describe('We expect all functions of RecursiveReplacement to work',
+    function() {
+
+        describe('We can detect if there are replacements in a string',
+            function() {
+                xit('true is true', function() {
+                    expect(false).toEqual(true)
+                })
+            }
+        );
+
+        describe('We can find a replacement in RecursiveData',
+            function() {
+                xit('true is true', function() {
+                    expect(false).toEqual(true)
+                })
+            }
+        );
+
+        describe('We can replace a replacement in a string',
+            function() {
+                xit('true is true', function() {
+                    expect(false).toEqual(true)
+                })
+            }
+        );
+
+        describe('We can replace a replacement in replacement in a string',
+            function() {
+                xit('true is true', function() {
+                    expect(false).toEqual(true)
+                })
+            }
+        );
+    }
+);
