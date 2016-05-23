e31f54ffc391a0976fcc354b59bbe564681b1b65
Daniel Mason <daniel@danielmason.com>
2016-05-23 22:42:46 +0100

    Next test.

--- tests/recursive-replacement.spec.js
+++ tests/recursive-replacement.spec.js
@@ -43,8 +43,50 @@ describe('We expect all functions of RecursiveReplacement to work',
             );
         });
 
-        xit('We can find a replacement in RecursiveData', function() {
-            assert.isTrue(false);
+        it('We can find a replacement in RecursiveData', function() {
+            let replacements = null;
+
+            replacements = RecursiveReplacement.getReplacementsInString(
+                'This string contains no replacements'
+            );
+            assert.isArray(replacements);
+            assert.lengthOf(replacements, 0);
+
+            replacements = RecursiveReplacement.getReplacementsInString(
+                'This string contains {{ one }} replacement'
+            );
+            assert.isArray(replacements);
+            assert.lengthOf(replacements, 1);
+            assert.include(replacements, 'one');
+
+            replacements = RecursiveReplacement.getReplacementsInString(
+                'This string contains one replacement with {{nospace}}'
+            );
+            assert.isArray(replacements);
+            assert.lengthOf(replacements, 1);
+            assert.include(replacements, 'nospace');
+
+            replacements = RecursiveReplacement.getReplacementsInString(
+                'This string {{ contains }} {{ two }} replacements'
+            );
+            assert.isArray(replacements);
+            assert.lengthOf(replacements, 2);
+            assert.include(replacements, 'contains');
+            assert.include(replacements, 'two');
+
+            replacements = RecursiveReplacement.getReplacementsInString(
+                'This string contains bb different dd mathcing replacements', 'bb dd'
+            );
+            assert.isArray(replacements);
+            assert.lengthOf(replacements, 1);
+            assert.include(replacements, 'different');
+
+            replacements = RecursiveReplacement.getReplacementsInString(
+                'This string contains {{ no }} mathcing replacements', 'bb dd'
+            );
+            assert.isArray(replacements);
+            assert.lengthOf(replacements, 0);
+
         });
 
         xit('We can replace a replacement in a string', function() {
